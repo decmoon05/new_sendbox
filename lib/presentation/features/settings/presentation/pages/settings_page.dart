c3e8f42689080197c9a335cbed421734e9c5296c
decmoon05 <decmoon05@naver.com>
2025-12-22 05:46:47 +0900

    fix: Update settings page to use SettingsService directly and add platformSettings route

--- lib/presentation/features/settings/presentation/pages/settings_page.dart
+++ lib/presentation/features/settings/presentation/pages/settings_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/di/providers.dart';
+import '../../../../../core/services/settings_service.dart';
+import '../../../../routes/route_names.dart';
 
 /// 설정 페이지
 class SettingsPage extends ConsumerWidget {
