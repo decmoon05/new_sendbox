6bbe4493684e853b4fd4a595ec91438604056f9c
decmoon05 <decmoon05@naver.com>
2025-12-21 23:59:00 +0900

    fix: Add missing Firebase import to providers

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
 import 'package:isar/isar.dart';
